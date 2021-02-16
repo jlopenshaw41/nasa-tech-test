3fe96956467f3d70827f131d0e0d1c7bc357d5b5
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-16 11:47:06 +0000

    Added test to check rendering of logo

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,21 @@
-import { getByTestId, render } from '@testing-library/react';
-import App from '../components/App';
+import { render } from "@testing-library/react";
+import App from "../components/App";
 
 describe("<App />", () => {
+  it("renders and matches snapshot", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   it("renders as expected", () => {
     const { getByTestId } = render(<App />);
     const app = getByTestId("app");
     expect(app).toBeTruthy();
+  });
 
+  it("renders logo", () => {
+    const { getByTestId } = render(<App />);
+    const logo = getByTestId("logo");
+    expect(logo).toBeTruthy();
   })
-})
+});
--- src/__tests__/Search.test.js
+++ src/__tests__/Search.test.js
@@ -2,6 +2,11 @@ import { render } from "@testing-library/react";
 import Search from "../components/Search";
 
 describe("<Search />", () => {
+  it("renders and matches snapshot", () => {
+    const { asFragment } = render(<Search />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   it("renders as expected", () => {
     const { getByTestId } = render(<Search />);
     const input = getByTestId("search-input");

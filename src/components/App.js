5e09cba4402120358e6d2328718c8ea1db05dbf0
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-16 12:03:29 +0000

    Amended styles as per dashboard and added event handler to search input

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,12 @@ import Search from "./Search.js";
 function App() {
   return (
     <div className="App" data-testid="app">
-      <img className="logo" src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" alt="NASA logo" data-testid="logo" />
+      <img
+        className="logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="NASA logo"
+        data-testid="logo"
+      />
       <Search />
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import "../styles/Search.css";
 
 const Search = () => {
+  const [value, setValue] = useState("");
   return (
     <>
-      <input className="search-input" type="text" data-testid="search-input"></input>
+      <input className="search-input" type="text" data-testid="search-input" onChange={(e) => setValue(e.target.value)}></input>
     </>
   );
 };
